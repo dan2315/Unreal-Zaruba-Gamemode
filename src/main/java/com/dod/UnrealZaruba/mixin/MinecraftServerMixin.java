a685082138b866a6b238d2c7a50b82faab6eae58
dan2315 <danilka2315.g@gmail.com>
2025-04-12 18:32:58 +0500

    logs removed

--- src/main/java/com/dod/UnrealZaruba/mixin/MinecraftServerMixin.java
+++ src/main/java/com/dod/UnrealZaruba/mixin/MinecraftServerMixin.java
@@ -6,7 +6,12 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import com.dod.UnrealZaruba.ConfigurationManager.ConfigManager;
 import com.dod.UnrealZaruba.api.IMinecraftServerExtended;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
@@ -20,13 +25,6 @@ public abstract class MinecraftServerMixin implements IMinecraftServerExtended {
     @Final
     private Map<ResourceKey<Level>, ServerLevel> levels;
     
-    /**
-     * Adds a new level to the server's level collection.
-     * This method exposes the shadowed levels map to allow adding custom dimensions.
-     * 
-     * @param levelKey The ResourceKey of the level to add
-     * @param level The ServerLevel instance to add
-     */
     @Override
     public void addLevel(ResourceKey<Level> levelKey, ServerLevel level) {
         this.levels.put(levelKey, level);
@@ -35,5 +33,13 @@ public abstract class MinecraftServerMixin implements IMinecraftServerExtended {
     @Override
     public void deleteLevel(ResourceKey<Level> levelKey) {
         this.levels.remove(levelKey);
+    }
+
+    @Inject(method = "saveAllChunks", at = @At("HEAD"), cancellable = true)
+    public void saveAllLevels(CallbackInfoReturnable<Boolean> ci) {
+        if (!ConfigManager.isDevMode()) {
+            ci.setReturnValue(false);
+            ci.cancel();
+        }
     }
 } 
